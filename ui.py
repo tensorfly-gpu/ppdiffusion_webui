2ffabd34b5f8dda06a7ded03c1540139acb38ada
风飏 <suixin812@hotmail.com>
2022-11-11 15:36:32 +0800

    更新SuperResolutionUI

--- ui.py
+++ ui.py
@@ -625,24 +625,33 @@ class SuperResolutionUI(StableDiffusionUI):
         super().__init__(**kwargs)
         widget_opt = self.widget_opt
         
+        layoutCol12 = Layout(
+            flex = "12 12 90%",
+            margin = "0.5em",
+            align_items = "center"
+        )
+        styleDescription = {
+            'description_width': "9rem"
+        }
+        
         widget_opt['image_path'] = widgets.Text(
-            layout=layout, style=style,
+            layout=layoutCol12, style=styleDescription,
             description='需要超分的图片路径' ,
             value='resources/Ring.png',
             disabled=False
         )
 
         widget_opt['superres_model_name'] = widgets.Dropdown(
-            layout=layout, style=style,
-            description='超分模型的名字' + '&nbsp;'*6,
+            layout=layoutCol12, style=styleDescription,
+            description='超分模型的名字',
             value="falsr_a",
             options=["falsr_a", "falsr_b", "falsr_c"],
             disabled=False
         )
 
         widget_opt['output_dir'] = widgets.Text(
-            layout=layout, style=style,
-            description='图片的保存路径' + '&nbsp;'*6,
+            layout=layoutCol12, style=styleDescription,
+            description='图片的保存路径',
             value="outputs/highres",
             disabled=False
         )
@@ -656,8 +665,19 @@ class SuperResolutionUI(StableDiffusionUI):
         )
         self.run_button.on_click(self.on_run_button_click)
 
-        self.gui = widgets.VBox([widget_opt[k] for k in list(widget_opt.keys())]
-                            +   [self.run_button, self.run_button_out])
+        self.gui = widgets.Box([
+            widget_opt['image_path'],
+            widget_opt['superres_model_name'],
+            widget_opt['output_dir'],
+            self.run_button,
+            self.run_button_out
+        ], layout = Layout(
+            display = "flex",
+            flex_flow = "row wrap", #HBox会覆写此属性
+            align_items = "center",
+            # max_width = '100%',
+            margin="0 45px 0 0"
+        ))
 
         self.task = 'superres'
 
